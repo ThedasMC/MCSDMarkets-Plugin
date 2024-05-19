a13e9cb50aa3559780be2e875b8ea1f50ca35541
tchristofferson <longleadtrc@gmail.com>
2024-05-19 16:50:45 -0500

    Small changes

--- src/main/java/com/thedasmc/mcsdmarketsplugin/support/Constants.java
+++ src/main/java/com/thedasmc/mcsdmarketsplugin/support/Constants.java
@@ -12,6 +12,6 @@ public class Constants {
     public static final String BUY_COMMAND_PERMISSION = "mcsd.buy";
     public static final String CREATE_CONTRACT_PERMISSION = "mcsd.contract.create";
     public static final String WITHDRAW_CONTRACT_PERMISSION = "mcsd.contract.withdraw";
-    public static final String VIEW_COMMAND_PERMISSION = "mcsdmarkets.view";
+    public static final String VIEW_COMMAND_PERMISSION = "mcsd.view";
 
 }
